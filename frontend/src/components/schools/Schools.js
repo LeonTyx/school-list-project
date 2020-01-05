cf1b739b73f3672f4e15978940511416fbc8aa72
LeonTyx <leontyx@gmail.com>
2020-01-04 16:14:08 -0800

    Added error handling for empty school list

--- frontend/src/components/schools/Schools.js
+++ frontend/src/components/schools/Schools.js
@@ -11,6 +11,9 @@ function Schools(){
             const response = await fetch("./api/v1/schools");
             const json = await response.json();
             setSchools(json);
+            if(json.schools === null && json.district === ""){
+
+            }
             setLoading(false)
         }
 
@@ -19,18 +22,23 @@ function Schools(){
     //todo: add "no schools" in case of empty school list
     return(
         !loading ?(
-            <div className="schools">
-                <img src={LandingHero} alt="Child ready for first day of school"/>
-                <h2>Schools in {schools.district}</h2>
+            json.schools === null && json.district === "" ? (
+                <div className="schools">
+                    <img src={LandingHero} alt="Child ready for first day of school"/>
+                    <h2>Schools in {schools.district}</h2>
 
-                <ul>
-                    {schools.schools.map(school => (
-                        <li key={school.school_id}>
-                            <a href={".#/lists/"+school.school_id}>List for {school.name}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                    <ul>
+                        {schools.schools.map(school => (
+                            <li key={school.school_id}>
+                                <a href={".#/lists/"+school.school_id}>List for {school.name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ):(
+                <div>Looks like this district doesn't have any schools</div>
+            )
+
         ):(
             <div>Loading...</div>
         )
